1dbd12bb16e04da538b64674606478830171c6b1
Paul MUSTIÈRE <paul.mustiere@gmail.com>
2017-01-18 12:32:28 +0100

    Dummy generation task for a sin curve

--- main.py
+++ main.py
@@ -8,17 +8,17 @@ from models.clockwork_rnn import ClockworkRNN
 if __name__ == '__main__':
     ### Create the Clockwork RNN ###
     config = {
-        'input_dim': 2,
+        'input_dim': 0,
         'hidden_dim': 90,
         'output_dim': 1,
         'periods': [1, 2, 4, 8, 16, 32, 64, 128, 256],
         'num_steps': 100,
 
         'learning_rate': 3e-4,
-        'learning_rate_step': 100,
+        'learning_rate_step': 200,
         'learning_rate_decay': 0.95,
         'momentum': 0.95,
-        'max_epochs': 200
+        'max_epochs': 2000
     }
 
     clockworkRNN = ClockworkRNN(config)
@@ -31,10 +31,16 @@ if __name__ == '__main__':
         # Create a writer
         log_writer = tf.train.SummaryWriter('log', sess.graph, flush_secs = 2)
 
+        # Load data
+        # Dummy data for now
+        data_dict = {
+            clockworkRNN.initial_state: np.zeros((config['hidden_dim'],)),
+            clockworkRNN.inputs: np.zeros((config['num_steps'], config['input_dim'])),
+            clockworkRNN.targets: np.reshape(np.sin(np.arange(config['num_steps'])), (config['num_steps'], config['output_dim']))
+        }
+
         for epoch in range(config['max_epochs']):
             results = sess.run([clockworkRNN.train_step, clockworkRNN.summaries],
-                feed_dict = {clockworkRNN.initial_state: np.ones((config['hidden_dim'],)),
-                            clockworkRNN.inputs: np.ones((config['num_steps'], config['input_dim'])),
-                            clockworkRNN.targets: np.zeros((config['num_steps'], config['output_dim']))})
+                feed_dict = data_dict)
 
             log_writer.add_summary(results[1], global_step = tf.train.global_step(sess, clockworkRNN.global_step))
