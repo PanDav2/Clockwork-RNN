90136f60b4a9e5cf10f1277c81176673b493b4c3
Pandav2 <david.panou@gmail.com>
2017-01-18 12:28:10 +0100

    added show signal

--- preprocess.py
+++ preprocess.py
@@ -1,6 +1,8 @@
 from __future__ import division
 import scipy.io.wavfile
-from numpy import abs, sign
+from numpy import abs, sign,array
+from collections import Counter
+import matplotlib.pyplot as plt 
 
 ##rate, signal = scipy.io.wavfile.read('data/gen/bad_taste.wav')
 
@@ -22,15 +24,27 @@ class preprocess(object):
     		if value>1:
     			return 1*s
     		else :
-    			return value
-    	self.nsignal = np.array(map(truncated,self.signal/2**encoding))
+    			return value*s
+    	self.nsignal = array(map(truncated,self.signal/2**encoding))
     	if verbose :
     		print(self.nsignal)
+    
+    def show_signal(self):
+    	if self.nsignal != None:
+    		signal = self.nsignal
+    	else:
+    		signal = self.signal
+    	plt.figure()
+    	plt.plot(signal,"-x")
+    	plt.show()
+
 
 def main():
 	p = preprocess(DEFAULT_FILENAME)
 	print(p.rate)
 	p.normalize()
+	p.show_signal()
+	#print(Counter(p.nsignal).most_common()[:10])
 
 if __name__ == '__main__':
     main()
